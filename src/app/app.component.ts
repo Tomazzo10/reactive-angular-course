403a63a11699ff62b6e81280bc43bfb425e3c019
Tomazzo Antolioni <tomazzoantolioni@MacBook-Pro-de-Tomazzo-Antolioni.local>
2022-06-07 17:52:00 +0200

    added logic to logout and the authstore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from "@angular/core";
 import { LoadingService } from "./loading/loading.service";
 import { MessagesService } from "./messages/messages.service";
+import { AuthStore } from "./services/auth.store";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
-  providers: [LoadingService, MessagesService],
+  providers: [],
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  constructor(public auth: AuthStore) {}
 
   ngOnInit() {}
 
-  logout() {}
+  logout() {
+    this.auth.logout();
+  }
 }
