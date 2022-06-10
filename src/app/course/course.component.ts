625510749a5a128cb2cbaf35382736307e57d508
Tomazzo Antolioni <tomazzoantolioni@MBPdeTontolioni.home>
2022-06-10 14:45:59 +0200

    refactored to have only one variable for observable data

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,5 +1,6 @@
 import {
   AfterViewInit,
+  ChangeDetectionStrategy,
   Component,
   ElementRef,
   OnInit,
@@ -21,19 +22,30 @@ import {
   shareReplay,
   catchError,
 } from "rxjs/operators";
-import { merge, fromEvent, Observable, concat, throwError } from "rxjs";
+import {
+  merge,
+  fromEvent,
+  Observable,
+  concat,
+  throwError,
+  combineLatest,
+} from "rxjs";
 import { Lesson } from "../model/lesson";
 import { CoursesService } from "../services/courses.service";
+
+interface CourseData {
+  course: Course;
+  lessons: Lesson[];
+}
 
 @Component({
   selector: "course",
   templateUrl: "./course.component.html",
   styleUrls: ["./course.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CourseComponent implements OnInit {
-  course$: Observable<Course>;
-
-  lessons$: Observable<Lesson[]>;
+  data$: Observable<CourseData>;
 
   constructor(
     private route: ActivatedRoute,
@@ -43,6 +55,22 @@ export class CourseComponent implements OnInit {
   ngOnInit() {
     const courseId = parseInt(this.route.snapshot.paramMap.get("courseId"));
 
-    this.course$ = this.coursesService.loadCourseById(courseId);
+    const course$ = this.coursesService
+      .loadCourseById(courseId)
+      .pipe(startWith(null));
+
+    const lessons$ = this.coursesService
+      .loadAllCourseLessons(courseId)
+      .pipe(startWith([]));
+
+    this.data$ = combineLatest([course$, lessons$]).pipe(
+      map(([course, lessons]) => {
+        return {
+          course,
+          lessons,
+        };
+      }),
+      tap(console.log)
+    );
   }
 }
