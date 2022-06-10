3fa03764db05edf1e3ec4313e8ed5b936e022ad4
Tomazzo Antolioni <tomazzoantolioni@MBPdeTontolioni.home>
2022-06-10 14:47:20 +0200

    added methods to load couse by id and to load all lessons

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -18,6 +18,20 @@ export class CoursesService {
       .pipe(shareReplay());
   }
 
+  loadAllCourseLessons(courseId: number): Observable<Lesson[]> {
+    return this.http
+      .get<Lesson[]>("/api/lessons", {
+        params: {
+          pageSize: "10000",
+          courseId: courseId.toString(),
+        },
+      })
+      .pipe(
+        map((res) => res["payload"]),
+        shareReplay()
+      );
+  }
+
   // Check get <Course[]> !!!
   loadAllCourses(): Observable<Course[]> {
     return this.http.get<Course[]>("/api/courses").pipe(
